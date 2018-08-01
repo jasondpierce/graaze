ea67dfebb939864ba53edfda9ce207c0ad4c7bff
jasonp <jasonp@graazesoft.com>
2018-08-01 15:45:11 -0400

    Heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,10 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile += %w( css/bootstrap.min.css)
+Rails.application.config.assets.precompile += %w( css/animate.css)
+Rails.application.config.assets.precompile += %w( css/style.css)
+Rails.application.config.assets.precompile += %w( css/themify-icons.css)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
